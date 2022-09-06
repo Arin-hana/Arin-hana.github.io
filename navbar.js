4f8cbcbb96fff16c6e41e3a278e71187e772fcbb
Arin Hanabi <91265178+Arin-hana@users.noreply.github.com>
2022-09-06 12:42:39 +0700

    Add files via upload

---
+++ navbar.js
@@ -0,0 +1,20 @@
+//sidenav
+function openNav() {
+  document.getElementById("sidepannel").style.width = "200px";
+}
+
+/* Set the width of the sidebar to 0 (hide it) */
+function closeNav() {
+  document.getElementById("sidepannel").style.width = "0";
+} 
+/* When the user scrolls down, hide the navbar. When the user scrolls up, show the navbar */
+var prevScrollpos = window.pageYOffset;
+window.onscroll = function() {
+  var currentScrollPos = window.pageYOffset;
+  if (prevScrollpos > currentScrollPos) {
+    document.getElementById("navbar").style.top = "0";
+  } else {
+    document.getElementById("navbar").style.top = "-50px";
+  }
+  prevScrollpos = currentScrollPos;
+}
