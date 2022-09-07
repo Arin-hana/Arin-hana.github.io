32fd5e4eabcd500acebdf0538bc88d3c795ab911
Arin Hanabi <91265178+Arin-hana@users.noreply.github.com>
2022-09-07 08:05:05 +0700

    Delete background.js

--- background.js
+++
@@ -1,7 +0,0 @@
-var ready = (callback) => {
-    if (document.readyState != "loading") callback();
-    else document.addEventListener("DOMContentLoaded", callback);
-}
-ready(() => {
-    document.querySelector(".bg").style.height = window.innerHeight + "px";
-})
